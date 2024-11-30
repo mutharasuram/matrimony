6074d14800d200d9cf032dabce1b41e928914d71
mutharasuram <mutharasuram0@gmail.com>
2024-11-30 23:42:21 +0530

    updated mock data

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
   
 use App\Http\Controllers\API\RegisterController;
-use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\MatchesController;
    
 Route::controller(RegisterController::class)->group(function(){
