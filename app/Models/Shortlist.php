498c89b88f7e93eb89da6d5c6b3ecf817f4c6984
mutharasuram <mutharasuram0@gmail.com>
2024-12-01 17:59:36 +0530

    shorted list model

---
+++ app/Models/Shortlist.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shortlist extends Model
+{
+    use HasFactory;
+
+    protected $table = 'shortlists';
+
+    protected $fillable = [
+        'user_id',
+        'shorted_id',
+    ];
+
+    /**
+     * Get the user who created the shortlist.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the user who is shortlisted.
+     */
+    public function shortlistedUser()
+    {
+        return $this->belongsTo(User::class, 'shorted_id');
+    }
+
+    /**
+     * Get the users that the current user has shortlisted.
+     *
+     * @param int $userId
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function iShortlisted(int $userId)
+    {
+        return self::where('user_id', $userId)->with('shortlistedUser')->get();
+    }
+
+    /**
+     * Get the users who have shortlisted the current user.
+     *
+     * @param int $userId
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function whoShortlistedMe(int $userId)
+    {
+        return self::where('shorted_id', $userId)->with('user')->get();
+    }
+}
