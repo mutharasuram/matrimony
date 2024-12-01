1c843c6d15f7c9ac9be1a4e278f41062999128f5
mutharasuram <mutharasuram0@gmail.com>
2024-12-01 13:36:19 +0530

    Revert "config"
    This reverts commit b129b9187512cfb239c5f4bf20e9c239c3ce18fe.

--- config/sanctum.php
+++ config/sanctum.php
@@ -5,9 +5,9 @@ use Laravel\Sanctum\Sanctum;
 return [
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Stateful Domains
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | Requests from the following domains / hosts will receive stateful API
     | authentication cookies. Typically, these should include your local
@@ -17,14 +17,14 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,13.202.198.249,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Sanctum Guards
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | This array contains the authentication guards that will be checked when
     | Sanctum is trying to authenticate a request. If none of these guards
@@ -33,12 +33,12 @@ return [
     |
     */
 
-    'guard' => ['api'],  // Use 'api' guard for API authentication
+    'guard' => ['web'],
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Expiration Minutes
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | This value controls the number of minutes until an issued token will be
     | considered expired. This will override any values set in the token's
@@ -46,12 +46,12 @@ return [
     |
     */
 
-    'expiration' => null, // Optional, you can set a specific expiration time
+    'expiration' => null,
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Token Prefix
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | Sanctum can prefix new tokens in order to take advantage of numerous
     | security scanning initiatives maintained by open source platforms
@@ -64,9 +64,9 @@ return [
     'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Sanctum Middleware
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | When authenticating your first-party SPA with Sanctum you may need to
     | customize some of the middleware Sanctum uses while processing the
