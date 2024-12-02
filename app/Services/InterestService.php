929aa70ba104010fd7615028b9790558d6d816d6
prasaanth08 <prasaanth.se@gmail.com>
2024-12-02 10:22:47 -0800

    api update

--- app/Services/InterestService.php
+++ app/Services/InterestService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\Interest;
+
 class InterestService
 {
     /**
@@ -11,4 +13,53 @@ class InterestService
     {
         //
     }
-}
+    public function storeIntrest($data)
+    {
+        $receiver_id=$data['receiver_id'];
+        $sender_id=$data['sender_id'];
+        $status=$data['status'];
+        $message=$data['message'];
+        // $Interest = Interest::where(['receiver_id' => $receiver_id, 'sender_id' => $sender_id])->first();
+        // if ($Interest) {
+        //     if($Interest->status === 1){
+        //         $data = Interest::where([
+        //             'receiver_id' => $receiver_id,
+        //             'sender_id' => $sender_id])
+        //             ->update([
+        //             'receiver_id' => $receiver_id,
+        //             'sender_id' => $sender_id,
+        //             'status' => $status,
+        //             'message' => $message
+        //         ]);
+        //         $datas['message']='Not Interested Updated Successfully';
+        //         $datas['response']=true;
+        //         return $datas;
+        //     }elseif($Interest->status == 2){
+        //         $data = Interest::where([
+        //             'receiver_id' => $receiver_id,
+        //             'sender_id' => $sender_id])
+        //             ->update([
+        //             'receiver_id' => $receiver_id,
+        //             'sender_id' => $sender_id,
+        //             'status' => $status,
+        //             'message' => $message
+        //         ]);
+        //         $datas['message']='Interested Updated successfully';
+        //         $datas['response']=true;
+        //         return $datas;
+        //     }
+        // } else {
+            Interest::create([
+                'sender_id' => $sender_id,
+                'receiver_id' => $receiver_id,
+                'status' => $status,
+                'message' => $message,
+            ]);
+            $datas['message']='Interested stored successfully';
+            $datas['response']=true;
+            return $datas;
+        // }
+        
+    
+     }
+    }
